3d3940cc7f03fcc43fb1fa4a73c950e7e5cb9b7f
nickg2000 <89922962+nickg2000@users.noreply.github.com>
2023-07-12 20:25:43 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from "express"
 import mysql from "mysql"
 import cors from "cors"
+import userRoutes from "./routes/users.js"
+import authRoutes from "./routes/auth.js"
+import cookieParser from "cookie-parser"
+
+
+
 
 const app = express()
 
@@ -11,8 +17,14 @@ const db = mysql.createConnection({
     database: "videogamedatabase"
 })
 
-app.use(express.json())
+//Middlewares
+app.use(express.json()) 
 app.use(cors())
+app.use(cookieParser())
+
+app.use("/backend/users", userRoutes )
+app.use("/backend/auth", authRoutes )
+
 
 app.get("/", (req,res)=>{
     res.json("hello this is the backend")
