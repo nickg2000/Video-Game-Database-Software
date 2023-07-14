4b820dc232214fee3c9918794174f4dd4a79c704
nickg2000 <89922962+nickg2000@users.noreply.github.com>
2023-07-14 19:04:58 -0400

    backend

--- index.js
+++ index.js
@@ -18,8 +18,14 @@ const db = mysql.createConnection({
 })
 
 //Middlewares
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Credentials", true)
+    next()
+})
 app.use(express.json()) 
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:3000",
+}))
 app.use(cookieParser())
 
 app.use("/backend/users", userRoutes )
