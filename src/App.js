82b83466fb63e114fd5c69097374448938f8ea58
nickg2000 <89922962+nickg2000@users.noreply.github.com>
2023-08-02 16:39:17 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/authContext';
+
 import Add from "./pages/Add";
 import Videogames from "./pages/Videogames";
 import Update from "./pages/Update";
 import Registration from "./pages/Registration";
 import Login from "./pages/Login";
-import "./style.css";
-
+import "./style.css"
 
 function App() {
   return (
-    <div className="App">
-     <BrowserRouter>
-     <Routes>
-      <Route path="/" element ={<Videogames/>}/>  
-      <Route path="/add" element ={<Add/>}/> 
-      <Route path="/update/:id" element ={<Update/>}/>  
-      <Route path="/registration" element ={<Registration/>}/> 
-      <Route path="/login" element ={<Login/>}/> 
-     </Routes>
-     </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Videogames />} />
+            <Route path="/add" element={<Add />} />
+            <Route path="/update/:id" element={<Update />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
